e7f44eb267d312257625168af6f6151e77dbfc4c
Thaddäus Wiedemer <thaddaeus.wiedemer@gmail.com>
2023-04-17 05:07:13 +0200

    add visualization for RGB images, finish visualization on grid for all latent combinations

--- src/vis.py
+++ src/vis.py
@@ -2,7 +2,9 @@ from math import ceil, sqrt
 from pathlib import Path
 from typing import List, Tuple
 
+import matplotlib.patches as patches
 import matplotlib.pyplot as plt
+import matplotlib.transforms as mtrans
 import numpy as np
 import pandas as pd
 import seaborn as sb
@@ -56,21 +58,31 @@ def visualize_slots_and_output(
     fig = plt.figure(figsize=(n_cols * plot_size, n_rows * plot_size))
     fig.suptitle(title)
 
+    # plot as image if there are color channels, otherwise as heatmap
+    plot_rgb = output[0].dim() == 3
+
     for col in range(n_cols):
-        # each column uses the same min/max for the colormap
-        all_values = torch.cat([slot[col] for slot in slots] + [output[col]])
-        vmin = min(all_values.min(), -all_values.max())
-        vmax = max(all_values.max(), -all_values.min())
+        if not plot_rgb:
+            # each column uses the same min/max for the colormap
+            all_values = torch.cat([slot[col] for slot in slots] + [output[col]])
+            vmin = min(all_values.min(), -all_values.max())
+            vmax = max(all_values.max(), -all_values.min())
 
         # plot slots
         for row in range(n_rows - 1):
             ax = fig.add_subplot(n_rows, n_cols, row * n_cols + col + 1)
-            ax.imshow(_rectify(slots[row][col]), cmap="RdBu", vmin=vmin, vmax=vmax)
+            if plot_rgb:
+                ax.imshow(slots[row][col])
+            else:
+                ax.imshow(slots[row][col], cmap="RdBu", vmin=vmin, vmax=vmax)
             ax.axis("off")
 
         # plot output
         ax = fig.add_subplot(n_rows, n_cols, (n_rows - 1) * n_cols + col + 1)
-        ax.imshow(_rectify(output[col]), cmap="RdBu", vmin=vmin, vmax=vmax)
+        if plot_rgb:
+            ax.imshow(output[col])
+        else:
+            ax.imshow(output[col], cmap="RdBu", vmin=vmin, vmax=vmax)
         ax.axis("off")
 
     # add column titles
@@ -110,8 +122,41 @@ def visualize_slots_and_output(
     plt.show()
 
 
+def _plot_image_grid(axs, x):
+    # plot as image if there are color channels, otherwise as heatmap
+    plot_rgb = x.dim() == 4
+
+    if not plot_rgb:
+        vmin = min(x.min(), -x.max())
+        vmax = max(x.max(), -x.min())
+
+    for ax, _x in zip(axs, x):
+        if plot_rgb:
+            ax.imshow(_x)
+        else:
+            ax.imshow(_x, cmap="RdBu", vmin=vmin, vmax=vmax)
+        ax.axis("off")
+
+
+def _get_xy_between_plots(fig, axs):
+    # get bounding boxes
+    r = fig.canvas.get_renderer()
+    get_bbox = lambda ax: ax.get_tightbbox(r).transformed(fig.transFigure.inverted())
+    bboxes = np.array([get_bbox(ax) for ax in axs.flat], mtrans.Bbox).reshape(axs.shape)
+
+    # get min/max extent and coordinates between them
+    xmax = np.array([b.x1 for b in bboxes.flat]).reshape(axs.shape).max(axis=0)
+    xmin = np.array([b.x0 for b in bboxes.flat]).reshape(axs.shape).min(axis=0)
+    ymax = np.array([b.y1 for b in bboxes.flat]).reshape(axs.shape).max(axis=1)
+    ymin = np.array([b.y0 for b in bboxes.flat]).reshape(axs.shape).min(axis=1)
+    xs = np.c_[xmax[1:], xmin[:-1]].mean(axis=1)
+    ys = np.c_[ymax[1:], ymin[:-1]].mean(axis=1)
+    return xs, ys
+
+
 def visualize_output_grid(
     comp_func: CompositionalFunction,
+    latent_transform: callable = None,
     grid_size: int = 5,
     dims: Tuple[int, int] = None,
     plot_size: float = 3,
@@ -121,25 +166,71 @@ def visualize_output_grid(
     sb.set_theme("notebook", "whitegrid")
 
     D = comp_func.d_in
-    z = sample_latents(D, "face_grid", grid_size=grid_size, dims=dims)
-    x = comp_func(z).detach()
 
     if dims is not None:
-        fig = plt.figure(figsize=(grid_size * plot_size, grid_size * plot_size))
+        fig, axs = plt.subplots(
+            grid_size, grid_size, figsize=(grid_size * plot_size, grid_size * plot_size)
+        )
         fig.suptitle(title)
 
-        vmin = min(x.min(), -x.max())
-        vmax = max(x.max(), -x.min())
+        z = sample_latents(D, "face_grid", grid_size=grid_size, dims=dims)
+        if latent_transform is not None:
+            z = latent_transform(z)
+        x = comp_func(z).detach()
 
-        for i, _x in enumerate(x):
-            ax = fig.add_subplot(grid_size, grid_size, i + 1)
-            ax.imshow(_rectify(_x), cmap="RdBu", vmin=vmin, vmax=vmax)
-            ax.axis("off")
-
-        plt.show()
+        _plot_image_grid(axs.flatten(), x)
 
     else:
-        raise NotImplementedError()
+        n_grids = sum(D) - 1
+        fig, axs = plt.subplots(
+            n_grids * grid_size,
+            n_grids * grid_size,
+            figsize=(n_grids * grid_size * plot_size, n_grids * grid_size * plot_size),
+        )
+        fig.suptitle(title)
+        for ax in axs.flatten():
+            ax.axis("off")
+
+        for row in range(1, n_grids + 1):
+            for col in range(row):
+                z = sample_latents(D, "face_grid", grid_size=grid_size, dims=(row, col))
+                if latent_transform is not None:
+                    z = latent_transform(z)
+                x = comp_func(z).detach()
+
+                _axs = axs[
+                    (row - 1) * grid_size : row * grid_size,
+                    col * grid_size : (col + 1) * grid_size,
+                ]
+                _plot_image_grid(_axs.flatten(), x)
+
+        # add background in a checkerboard pattern
+        plt.tight_layout()
+        xs, ys = _get_xy_between_plots(fig, axs)
+        xs = [0] + list(xs[grid_size - 1 :: grid_size]) + [1]
+        ys = [1] + list(ys[grid_size - 1 :: grid_size]) + [0]
+
+        for row in range(n_grids):
+            for col in range(row):
+                # skip every 2nd cell
+                if (row + col) % 2 == 0:
+                    continue
+
+                w = xs[col + 1] - xs[col]
+                h = ys[row + 1] - ys[row]
+
+                patch = patches.Rectangle(
+                    (xs[col], ys[row]),
+                    w,
+                    h,
+                    edgecolor=None,
+                    facecolor="tab:blue",
+                    transform=fig.transFigure,
+                    zorder=0,
+                )
+                fig.add_artist(patch)
+
+    plt.show()
 
 
 def visualize_score_heatmaps(
